ffe8dd97ea7fc0d797a6a035a17d2f7c369b36c3
Admin <entropycritical@gmail.com>
2018-01-11 19:31:43 +0530

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,23 +10,42 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (msg) {
   var formattedTime = moment(msg.createdAt).format('LT');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    from: msg.from,
+    text: msg.text,
+    createdAt: formattedTime
+  });
 
-  var li = jQuery('<li></li>');
-  li.text(`${msg.from} ${formattedTime}: ${msg.text}`);
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
+  // var formattedTime = moment(msg.createdAt).format('LT');
+  //
+  // var li = jQuery('<li></li>');
+  // li.text(`${msg.from} ${formattedTime}: ${msg.text}`);
+  //
+  // jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('LT');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
 
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
-
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
+  // var formattedTime = moment(message.createdAt).format('LT');
+  //
+  // var li = jQuery('<li></li>');
+  // var a = jQuery('<a target="_blank">My current location</a>');
+  //
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href', message.url);
+  // li.append(a);
+  // jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function (e) {
